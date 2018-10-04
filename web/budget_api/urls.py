5ead85c7705e765d09ff53b544d87c5a1c5dcc32
MAX MCFARLAND <mmcfarland522@gmail.com>
2018-10-03 18:33:38 -0700

    adding api app

---
+++ web/budget_api/urls.py
@@ -0,0 +1,12 @@
+from .views import RegisterApiView, UserApiView
+from django.urls import path
+from rest_framework.authtoken import views
+
+
+urlpatterns = [
+    path('user/<int:pk>', UserApiView.as_view(), name='user-detail'),
+    path('register', RegisterApiView.as_view(), name='register'),
+    path('login', views.obtain_auth_token),
+    path('budget', BudgetApiView.as_view(), name='budget-list'),
+    path('transaction', TransactionApiView.as_view(), name='transaction-detail'),
+]
